78f9e411d0ed2396ea58aa23dceb1bb0beeafc81
Maksim <maksim_belotelov@mail.ru>
2022-11-25 21:40:35 +0300

    add Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа выводит третью цифру числа или сообщает, что третьей
+// цифры нет
+
+Console.WriteLine("Введите число: ");
+int num=int.Parse(Console.ReadLine());
+int numcopy=num;
+int den=1;
+int i=0, j=0;
+
+while(numcopy!=0) //считаем количество цифр в числе
+{
+    i++;
+    numcopy/=10;
+}
+
+if(i<3) // Если в числе меньше 3 цифр, третьей цифры нет     
+    Console.WriteLine($"В числе {num} нет третьей цифры");
+else
+{
+    Console.WriteLine($"В числе {num} {i} цифр");
+    while(j<(i-3)) //получаем нужную степень 10 для знаменателя
+    {
+        den*=10;
+        j++;
+    }
+    Console.WriteLine("Третья цифра: "+(num/den) % 10);
+}
