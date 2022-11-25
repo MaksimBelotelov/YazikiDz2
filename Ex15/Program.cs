922258782bba753e9c3f20cc5d7e86d957f72ba1
Maksim <maksim_belotelov@mail.ru>
2022-11-25 21:51:02 +0300

    add Ex15

---
+++ Ex15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает цифру, обозначающую день недели и проверяет
+// является ли он выходным
+
+Console.Clear();
+
+Console.Write("Введите номер дня недели: ");
+int num=int.Parse(Console.ReadLine());
+
+if(num > 7 || num < 1) 
+    Console.Write("В неделе только 7 дней!");
+else
+{
+    if(num > 0 && num < 6)
+        Console.Write("День рабочий");
+    else
+        Console.Write("День выходной");
+}
