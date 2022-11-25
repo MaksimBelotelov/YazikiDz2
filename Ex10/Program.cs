9f84608a91a62f3d810d9951614aa2965f9a1915
Maksim <maksim_belotelov@mail.ru>
2022-11-25 21:57:37 +0300

    refactor: deleted s in Ex10

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -5,12 +5,8 @@ Console.Clear();
 
 Console.Write("Введите трехзначное число: ");
 int num = int.Parse(Console.ReadLine());
-int s;
 
 if(num/1000!=0||num/100==0) 
     Console.Write("Вы ввели не трехзначное число!");
 else
-{
-    s=(num%100)/10;
-    Console.WriteLine($"Вторая цифра в числе: {s}");
-}
+    Console.WriteLine($"Вторая цифра в числе: "+(num%100)/10);
